06a7418784fecfeb996bf3323fa146e50273afd3
zia khan <ziaalikhan052@gmail.com>
2021-06-29 09:28:56 +0500

    Work complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Data from './CardsData';
+
+
 
 function App() {
+
+const [items, setItems] = useState(Data);
+ 
+
+const filterItem = (categoryItem) => {
+  const updatedItem = Data.filter((val) => {
+    return val.category === categoryItem;
+  })
+
+  setItems(updatedItem)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="headingContener">
+
+      <h3 className='head'>Photo Images Gallery </h3>
+      </div>
+      <div className=' allbuttons'>
+            <button className='btn1' onClick={() => filterItem('MensCloth') }>Mens Cloth</button>
+            <button className='btn1' onClick={() => filterItem('WomensCloth') }>Womens Cloth</button>
+            <button className='btn1' onClick={() => filterItem('BabyCloth') }>Boys Cloth</button>
+            <button className='btn1' onClick={() => filterItem('BoysCloth') }>Babys Cloth</button>
+            <button className='btn1' onClick={() => setItems(Data) }>All</button>
+            {/* <button className='btn1'>Mens Cloth</button> */}
+            
+        </div>
+    <div className="MainCards">
+
+      {
+        items.map((val) => {
+          return(
+            <div className="card mb-3" key={val.id} >
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src={val.images} className="img-fluid rounded-start" id='imagess' alt="..." />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">{val.title}</h5>
+                  <p className="card-text">{val.desc}.</p>
+                  <p>Price : {val.price}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          )
+        })
+      }
+
+
+
+       
+
+    </div>
     </div>
   );
 }
---
+++ src/CardsData.js
@@ -0,0 +1,164 @@
+const Data = [
+    {
+      id:0,
+      images: './images/mens1.jpg',
+      title: 'Mens Cloths',
+      desc: 'Mens casual clothes brands',
+      category: 'MensCloth',
+      price: '30$'
+    },
+    {
+      id:1,
+      images: './images/mens2.jpg',
+      title: 'Mens Cloths',
+      desc: 'Mens casual clothes brands',
+      category: 'MensCloth',
+      price: '30$'
+    },
+    {
+      id:2,
+      images: './images/mens3.jpg',
+      title: 'Mens Cloths',
+      desc: 'Mens casual clothes brands',
+      category: 'MensCloth',
+      price: '30$'
+    },
+    {
+      id:3,
+      images: './images/mens4.jpg',
+      title: 'Mens Cloths',
+      desc: 'Mens casual clothes brands',
+      category: 'MensCloth',
+      price: '30$'
+    },
+    {
+      id:4,
+      images: './images/mens5.jpg',
+      title: 'Mens Cloths',
+      desc: 'Mens casual clothes brands',
+      category: 'MensCloth',
+      price: '30$'
+    },
+    {
+      id:5,
+      images: './images/womenscloth1.jpg',
+      title: 'Womens Cloths',
+      desc: 'Womens casual clothes brands',
+      category: 'WomensCloth',
+      price: '30$'
+    },
+    {
+      id:6,
+      images: './images/womenscloth2.jpg',
+      title: 'Womens Cloths',
+      desc: 'Womens casual clothes brands',
+      category: 'WomensCloth',
+      price: '30$'
+    },
+    {
+      id:7,
+      images: './images/womenscloth3.jpg',
+      title: 'Womens Cloths',
+      desc: 'Womens casual clothes brands',
+      category: 'WomensCloth',
+      price: '30$'
+    },
+    {
+      id:8,
+      images: './images/womenscloth4.jpg',
+      title: 'Womens Cloths',
+      desc: 'Womens casual clothes brands',
+      category: 'WomensCloth',
+      price: '30$'
+    },
+    {
+      id:9,
+      images: './images/womenscloth5.jpg',
+      title: 'Womens Cloths',
+      desc: 'Womens casual clothes brands',
+      category: 'WomensCloth',
+      price: '30$'
+    },
+    {
+      id:10,
+      images: './images/babycloth1.jpg',
+      title: 'Baby Cloths',
+      desc: 'Baby casual clothes brands',
+      category: 'BabyCloth',
+      price: '30$'
+    },
+    {
+      id:11,
+      images: './images/babycloth2.jpg',
+      title: 'Baby Cloths',
+      desc: 'Baby casual clothes brands',
+      category: 'BabyCloth',
+      price: '30$'
+    },
+    {
+      id:12,
+      images: './images/babycloth3.jpg',
+      title: 'Baby Cloths',
+      desc: 'Baby casual clothes brands',
+      category: 'BabyCloth',
+      price: '30$'
+    },
+    {
+      id:13,
+      images: './images/babycloth4.jpg',
+      title: 'Baby Cloths',
+      desc: 'Baby casual clothes brands',
+      category: 'BabyCloth',
+      price: '30$'
+    },
+    {
+      id:14,
+      images: './images/babycloth5.jpg',
+      title: 'Baby Cloths',
+      desc: 'Baby casual clothes brands',
+      category: 'BabyCloth',
+      price: '30$'
+    },
+    {
+      id:15,
+      images: './images/boyscloth1.jpg',
+      title: 'Boys Cloths',
+      desc: 'Boys casual clothes brands',
+      category: 'BoysCloth',
+      price: '30$'
+    },
+    {
+      id:16,
+      images: './images/boyscloth2.jpg',
+      title: 'Boys Cloths',
+      desc: 'Boys casual clothes brands',
+      category: 'BoysCloth',
+      price: '30$'
+    },
+    {
+      id:17,
+      images: './images/boyscloth3.jpg',
+      title: 'Boys Cloths',
+      desc: 'Boys casual clothes brands',
+      category: 'BoysCloth',
+      price: '30$'
+    },
+    {
+      id:18,
+      images: './images/boyscloth4.jpg',
+      title: 'Boys Cloths',
+      desc: 'Boys casual clothes brands',
+      category: 'BoysCloth',
+      price: '30$'
+    },
+    {
+      id:19,
+      images: './images/boyscloth5.jpg',
+      title: 'Boys Cloths',
+      desc: 'Boys casual clothes brands',
+      category: 'BoysCloth',
+      price: '30$'
+    },
+  ]
+
+  export default Data;
